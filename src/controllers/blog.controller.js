79c1235e410c201693ffd45c44e5faa75bc1ff04
CglynDogan <caglayan.dogan96@gmail.com>
2026-01-22 22:21:46 +0300

    controller category update

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -3,7 +3,7 @@
 /* BLOG API CONTROLLES */
 
 require("express-async-errors");
-const { BlogPost } = require("../models/blog.model");
+const { BlogCategory, BlogPost } = require("../models/blog.model");
 
 // module.exports = {
 //   key: "value",
@@ -12,6 +12,49 @@ const { BlogPost } = require("../models/blog.model");
 
 // module.exports.key:"value"
 // module.exports.key2:"value"
+
+module.exports.BlogCategory = {
+  list: async (req, res) => {
+    const data = await BlogCategory.find();
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await BlogCategory.create(req.body);
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      data: data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await BlogCategory.find({ _id: req.params.categoryId });
+    res.status(202).send({
+      error: false,
+      data: data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await BlogCategory.updateOne(
+      { _id: req.params.postId },
+      req.body,
+    );
+    const newdata = await BlogCategory.find({ _id: req.params.categoryId });
+    res.status(202).send({
+      error: false,
+      data: data, // info about update
+      // guncel veriyi istiyorsan tekrar cagir
+      newdata: newdata,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await BlogCategory.deleteOne({ _id: req.params.categoryId });
+    // console.log(data);
+    res.sendStatus(data.deletedCount >= 1 ? 204 : 404).send({});
+  },
+};
 
 module.exports.BlogPost = {
   list: async (req, res) => {
