d35a3e0348d60b93993f7ad8b385594e3a4c863e
CglynDogan <caglayan.dogan96@gmail.com>
2026-01-23 13:32:30 +0300

    req sync done added the comment line

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ app.use("/blog", require("./src/routes/blog.route"));
 app.use(require("./src/middlewares/errorHandler"));
 app.listen(PORT, () => console.log(`Server Running on http://${HOST}:${PORT}`));
 
-require("./src/sync")();
+// require("./src/sync")();
