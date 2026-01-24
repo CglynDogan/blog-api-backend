f22e5ac1f88fb5f98e5bdad3806349154d019a44
CglynDogan <caglayan.dogan96@gmail.com>
2026-01-24 13:07:01 +0300

    password encrpyt up

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,28 +1,43 @@
 "use strict";
-
-// EXPRESSJS -- BLOG API //
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG API
+------------------------------------------------------- */
 
 const mongoose = require("mongoose");
 
-// Schema:
+//  Password Encryption:
+//  https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+// const crypto = require('node:crypto')
 
-const userSchema = new mongoose.Schema(
+// const keyCode = process.env?.SECRET_KEY || 'write_random_chars_in_here'
+// const loopCount = 10_000 // 10K
+// const charCount = 32 // write 32 for 64
+// const encType = 'sha512'
+
+// const passwordEncrypt = function (password) {
+//     return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+// }
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+// Schema:
+const UserSchema = new mongoose.Schema(
   {
     email: {
       type: String,
       trim: true,
-      // require: true,
-      // unique: [true, "Email must be unique."],
+      unique: true,
+      // unique: [true, 'Email must be unique.'], // Not support
+      // required: true,
       required: [true, "Email must be required."],
-      // validate: (email) => {return true}
+      // validate: (email) => { return true },
       // validate: [
-      //   (email) => {
-      //     if (email.includes("@") && email.includes(".")) {
-      //       return true;
-      //     }
-      //     return false;
-      //   },
-      //   "Email type is incorrect",
+      //     (email) => {
+      //         if (email.includes('@') && email.includes('.')) {
+      //             return true
+      //         }
+      //         return false
+      //     },
+      //     'Email type is incorrect'
       // ],
       validate: [
         (email) => email.includes("@") && email.includes("."),
@@ -33,7 +48,11 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       trim: true,
-      require: true,
+      required: true,
+      // set: (password) => { return password + '123' },
+      // set: function (password) { return password + '123' },
+      set: (password) => passwordEncrypt(password),
+      // set: passwordEncrypt
     },
 
     firstName: String,
@@ -50,4 +69,4 @@ const userSchema = new mongoose.Schema(
 //     User: mongoose.model('User', UserSchema)
 // }
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
